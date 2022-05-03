40c51e629ff483d8ce70607499553d74f0cfb2ee
CoastRed <Coastline.alex7315@gmail.com>
2022-05-03 23:00:26 +0800

    添加项目文件。

---
+++ Dark Admin Panel/UserControls/InfoCard.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dark_Admin_Panel.UserControls
+{
+    /// <summary>
+    /// InfoCard.xaml 的交互逻辑
+    /// </summary>
+    public partial class InfoCard : UserControl
+    {
+        public InfoCard()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(InfoCard));
+
+        public string Number
+        {
+            get { return (string)GetValue(NumberProperty); }
+            set { SetValue(NumberProperty, value); }
+        }
+
+        public static readonly DependencyProperty NumberProperty = DependencyProperty.Register("Number", typeof(string), typeof(InfoCard));
+
+        public FontAwesome.Sharp.IconChar Icon
+        {
+            get { return (FontAwesome.Sharp.IconChar)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(FontAwesome.Sharp.IconChar), typeof(InfoCard));
+
+        public Color Background1
+        {
+            get { return (Color)GetValue(Background1Property); }
+            set { SetValue(Background1Property, value); }
+        }
+
+        public static readonly DependencyProperty Background1Property = DependencyProperty.Register("Background1", typeof(Color), typeof(InfoCard));
+
+        public Color Background2
+        {
+            get { return (Color)GetValue(Background2Property); }
+            set { SetValue(Background2Property, value); }
+        }
+
+        public static readonly DependencyProperty Background2Property = DependencyProperty.Register("Background2", typeof(Color), typeof(InfoCard));
+
+        public Color EllipseBackground1
+        {
+            get { return (Color)GetValue(EllipseBackground1Property); }
+            set { SetValue(EllipseBackground1Property, value); }
+        }
+
+        public static readonly DependencyProperty EllipseBackground1Property = DependencyProperty.Register("EllipseBackground1", typeof(Color), typeof(InfoCard));
+
+        public Color EllipseBackground2
+        {
+            get { return (Color)GetValue(EllipseBackground2Property); }
+            set { SetValue(EllipseBackground2Property, value); }
+        }
+
+        public static readonly DependencyProperty EllipseBackground2Property = DependencyProperty.Register("EllipseBackground2", typeof(Color), typeof(InfoCard));
+
+
+    }
+}
